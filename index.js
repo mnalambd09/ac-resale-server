cb943514ea99f3eaf2a8da3fbb6629af0227ae9d
mnalambd09 <mnalambd09@gmail.com>
2022-11-30 02:07:11 +0600

    data loadded by categories

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const categories = require('./Categories.json');
+const product = require('./Product.json')
 
 app.get('/', async(req, res) => {
     res.send('AC Resale Server is running');
@@ -16,5 +17,12 @@ app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+
+app.get('/product/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedProduct = product.filter(pro => pro.category === id);
+    res.send(selectedProduct)
+    console.log(req.params.id);
+})
 
 app.listen(port, () => console.log(`AC Resale is running on port ${port}`))
