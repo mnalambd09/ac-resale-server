b53f31ba2430cf31952599be72f16f82090e00b2
mnalambd09 <mnalambd09@gmail.com>
2022-11-30 00:41:54 +0600

    categories added

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+const categories = require('./Categories.json');
 
 app.get('/', async(req, res) => {
     res.send('AC Resale Server is running');
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
 
 
 app.listen(port, () => console.log(`AC Resale is running on port ${port}`))
